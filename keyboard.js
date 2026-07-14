e53682b2b781af58f7a8a3536fbbec9968606872
Safi <agussupriyadi987654@gmail.com>
2026-07-14 12:37:34 +0700

    Add files via upload

---
+++ keyboard.js
@@ -0,0 +1,32 @@
+// Fungsi Memunculkan Keyboard dan 
+// mengambil input keyboard di mobile
+
+let hidInput = document.createElement('input');
+hidInput.style = "width:0px; height:0px;"
+hidInput.style.position = "absolute";
+hidInput.style.left = "-9999px"; 
+document.body.appendChild(hidInput);
+//hidInput.style = "opacity:0;";
+hidInput.value = "    ";
+
+function getChar(e) {
+    return new Promise(resolve => {
+        if (e.key !== "Unidentified" && e.key.length === 1){  
+            resolve(e.key);  
+            hidInput.value = "    ";
+            hidInput.setSelectionRange(2,2);
+        } else {
+            setTimeout(() => {
+                let char = hidInput.value.trim();
+                resolve(char); 
+                hidInput.value = "    ";   
+                hidInput.setSelectionRange(2,2); 
+            }, 20); 
+        }
+    });
+}
+
+window.addEventListener("click",()=>{
+	hidInput.focus();
+	hidInput.setSelectionRange(2,2);
+});
